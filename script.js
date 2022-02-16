43ba5e594b4f55c359fd246fecb3a418be5a9d6b
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-02-15 20:36:21 -0600

    Updates

---
+++ script.js
@@ -0,0 +1,12 @@
+//animate landing page loader
+let landing = document.getElementById('landing-page')
+//To set time interval for black background
+//Could have done this with setTimeout()
+setInterval(function(){
+    landing.style.opacity= "0"}, 3000)
+//To set time interval to remove landing page
+setInterval(()=>
+    landing.style.display = "none",3300)
+
+console.log(landing)
+//-------------------------------------------------------------------------//
