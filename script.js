94debc3145b380fdd1454d37231f88a9c4de658c
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-02-16 17:23:33 -0600

    Got the image in

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let doc = document.documentElement;
 //Page Setup
 //------------------------------------------------------------------------------//
 //animate landing page loader
@@ -24,8 +23,8 @@ import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examp
 
 //Set up Slider Events
 //Declare sliders//
-let image, width, height;
-
+//Set up Image Selection and Preview--------------------------------------//
+let image;
 image = document.getElementById("myImage")
 const imagePreview = document.getElementsByClassName("image_preview")[0];
 image.addEventListener('change', imageChange);
@@ -37,8 +36,8 @@ function imageChange(){
     while(imagePreview.firstChild){
         console.log(imagePreview.firstChild)
         console.log(imagePreview.innerHTML)
-
         imagePreview.removeChild(imagePreview.firstChild)
+    }
     if (image.files.length === 0){
         alert('No Image Selected')
         imagePreview.innerHTML="No Image Selected"
@@ -50,8 +49,7 @@ function imageChange(){
 }
 
 function updateImageDisplay(){
-
-    }
+    
     let image2 = document.createElement('img');
     imagePreview.appendChild(image2);
     if(validFileType(image.files[0])){
@@ -64,7 +62,158 @@ const fileTypes = ["image/png", "image/jpeg"];
 function validFileType(file) {
     return fileTypes.includes(file.type);
   }
-//image.setAttribute ('onchange', "imageChange()")
+//-----------------------------------------------------------//
 
-//imageChange()
+//Setup Parameter Sliders-----------------------------------------//
+let width, height;
+
+width = document.getElementById("width")
+width.addEventListener('click',  onSliderChange,false)
+width.addEventListener('touchend',  onSliderChange,false)
+
+height = document.getElementById("height")
+height.addEventListener('click', onSliderChange,false)
+height.addEventListener('touchend', onSliderChange,false)
+
+
 const definitionName = 'Portraits.gh';
+const loader = new Rhino3dmLoader()
+loader.setLibraryPath('https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/')
+
+let rhino, definition, doc
+rhino3dm().then(async m => {
+    console.log('Loaded rhino3dm.')
+    rhino = m // global
+
+
+    //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
+    //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
+    
+    RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
+
+
+    // load a grasshopper file!
+    const url = definitionName
+    const res = await fetch(url)
+    const buffer = await res.arrayBuffer()
+    const arr = new Uint8Array(buffer)
+    definition = arr
+
+    init()
+    compute()
+})
+
+async function compute() {
+
+
+    const param1 = new RhinoCompute.Grasshopper.DataTree('Image Width')
+    param1.append([0], [width.valueAsNumber])
+    console.log(param1)
+
+    const param2 = new RhinoCompute.Grasshopper.DataTree('Image Height')
+    param2.append([0], [height.valueAsNumber])
+    console.log(param2)
+
+    // clear values
+    const trees = []
+    trees.push(param1)
+    trees.push(param2)
+
+    const res = await RhinoCompute.Grasshopper.evaluateDefinition(definition, trees)
+    //console.log(res)
+        
+
+
+    doc = new rhino.File3dm()
+
+    // hide spinner
+    document.getElementById('container').style.display = 'none'
+
+    for (let i = 0; i < res.values.length; i++) {
+
+        for (const [key, value] of Object.entries(res.values[i].InnerTree)) {
+            for (const d of value) {
+
+                const data = JSON.parse(d.data)
+                //console.log(data)
+                const rhinoObject = rhino.CommonObject.decode(data)
+                doc.objects().add(rhinoObject, null)
+
+            }
+        }
+    }
+
+
+    // clear objects from scene
+    scene.traverse(child => {
+        if (child.type === "Object3D") {
+            scene.remove(child)
+        }
+    })
+
+
+    const buffer = new Uint8Array(doc.toByteArray()).buffer
+    loader.parse(buffer, function (object) {
+
+        scene.add(object)
+        console.log(scene)
+        // hide spinner
+        document.getElementById('container').style.display = 'none'
+
+    })
+}
+
+
+function onSliderChange() {
+    // show spinner
+    document.getElementById('container').style.display = 'flex'
+    compute()
+}
+
+
+
+// BOILERPLATE //
+let scene, camera, renderer, controls
+
+function init() {
+
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+    
+
+
+    // create a scene and a camera
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0, 1, 0)
+    camera = new THREE.PerspectiveCamera(1000, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.z=50
+
+
+    // create the renderer and add it to the html
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    
+
+    // add some controls to orbit the camera
+    controls = new OrbitControls(camera, renderer.domElement)
+
+    const Axis = new THREE.AxesHelper(5)
+    scene.add(Axis)
+
+    // add a directional light
+    const directionalLight = new THREE.DirectionalLight(0xffffff)
+    directionalLight.intensity = 2
+    scene.add(directionalLight)
+
+    const ambientLight = new THREE.AmbientLight()
+    scene.add(ambientLight)
+
+    console.log(scene)
+
+    animate()
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
